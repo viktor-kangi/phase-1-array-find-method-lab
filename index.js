5a96a87a820a0e5553da31bc2940831701725c56
victor <victor.buthia@student.moringaschool.com>
2022-11-26 13:15:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [    
+    { year: "1971", result: "N/A"},
+    { year: "1970", result: "N/A"},
+    { year: "1969", result: "W"},
+    { year: "1968", result: "N/A"},
+    { year: "1967", result: "N/A"},
+    { year: "1966", result: "L"},
+    { year: "1965", result: "N/A"},
+    { year: "1964", result: "N/A"},
+    ];
+    
+function superbowlWin(record){
+    for(let item of record){
+        if(item.result === "W"){
+            return item.year;
+        }
+    }
+}
